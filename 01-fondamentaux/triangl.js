d4edeb87dccbf7a370a18e872f345c523903ad8a
walid-walid <its.n.chihab@gmail.com>
2025-04-19 01:10:16 +0100

    Update triangl.js
    update subject

--- 01-fondamentaux/triangl.js
+++ 01-fondamentaux/triangl.js
@@ -1,4 +1,7 @@
-/*Objectif : Créer un programme en JavaScript qui demande à l'utilisateur d'entrer la taille de son triangle. Ensuite, le programme doit afficher un triangle de # dont le nombre de lignes et de caractères par ligne est déterminé par l'utilisateur.*/
+/**Objectif : Créer un programme en JavaScript qui demande à l'utilisateur d'entrer la tail
+*le de son triangle. Ensuite, le programme doit afficher un triangle de # dont le nombre de li
+*gnes et de caractères par ligne est déterminé par l'utilisateur.
+**/
 
 let  length = prompt("entrer langeure de votre  triangle :");
 for (let i=0 ; i<length ; i++){
