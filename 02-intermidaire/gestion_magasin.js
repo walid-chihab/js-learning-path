ba0cb2d6c6fbb6d4c3459f24961b2a78cbc58051
walid-chihab <walid-chihab@users.noreply.github.com>
2025-05-07 23:29:19 +0100

    acheter

--- 02-intermidaire/gestion_magasin.js
+++ 02-intermidaire/gestion_magasin.js
@@ -7,16 +7,33 @@
 
 let produits= [];
 
+
+//push: ajout a la fin ,:unshift ajout au debut
 function ajoutProduit (nom , prix, stock){
-	produits.push({nom , prix ,stock)};
+	produits.push({nom , prix ,stock});
 }
 
 function affichProduit (){
 	if (produits.length === 0)
-		console.log ('votre magasn est telment vide hihi!');
+		console.log ('votre magasin est telment vide hihi!');
 	return;
 	produits.forEach((produit, index) => ){
 		console.log (`${index + 1}. ${produit.nom} - Prix: ${produit.prix}€, Stock: ${produit.stock}`);
   });
 }
+
+//achète une quantité d'un produit si possible
+void achterProduit(nom , quantite){
+	let produit = produit.find(p => p.nom === nom );
+	if (!produit)
+		console.log("ce produit n'existe pas");
+	return;
+	if (produit.stock < quantite ){
+		console.log("stock pas sufissant!");
+		return;
+	}
+	produit.stock -= quantite;
+	console.log (` l'achat de ${quantite} de nom ${nom} est effectue`);
+}
+
 
